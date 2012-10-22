648aafa2f2b124e348ac1cd77fda3391fc2f6774
Sergi Almacellas Abellana <sergi@koolpi.com>
2012-10-22 12:17:01 +0200

    Show categories from current company, not for all companies.

--- lib/form/ProductCategoriesForm.class.php
+++ lib/form/ProductCategoriesForm.class.php
@@ -4,7 +4,8 @@ class ProductCategoriesForm extends FormsContainer
 {
   public function __construct($options = array(), $CSRFSecret = null)
   {
-    $this->old_product_category = Doctrine::getTable('ProductCategory')->findAll();
+    $this->old_product_category = Doctrine::getTable('ProductCategory')->createQuery()
+    ->where('company_id = ?', sfContext::getInstance()->getUser()->getAttribute('company_id'))->execute();
   
     $forms = array();
     foreach ($this->old_product_category as $product_category)
