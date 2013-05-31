5a281bd4333aed34e77c9ed6498f3f41e1169cbc
Sergi Almacellas Abellana <sergi@koolpi.com>
2013-05-31 16:37:08 +0200

    Show username on users list.

--- apps/siwapp/modules/users/templates/indexSuccess.php
+++ apps/siwapp/modules/users/templates/indexSuccess.php
@@ -6,9 +6,9 @@ $csrf     = new sfForm();
 ?>
 
 <div class="content">
-  
+
   <?php if (count($users)): ?>
-    
+
     <?php echo form_tag('users/batch', 'id=batch_form class=batch') ?>
       <?php echo $csrf['_csrf_token']->render(); ?>
       <input type="hidden" name="batch_action" id="batch_action">
@@ -20,7 +20,7 @@ $csrf     = new sfForm();
           <tr class="empty noborder listing-options">
             <td colspan="2">
               <?php echo gButton_to_function(__("Delete"), "do_batch('delete')", array('class'=>'batch delete action-clear', 'confirm'=>__('Are you sure?'))) ?>
-            </td>   
+            </td>
           </tr>
 
           <tr class="empty noborder">
@@ -32,12 +32,13 @@ $csrf     = new sfForm();
             <?php
               // sort parameter => array (Name, default order)
               renderHeaders(array(
+                'username' => array('Username', 'asc'),
                 'first_name' => array('First name', 'asc'),
                 'last_name' => array('Last name', 'asc'),
                 'email'    => array('Email', 'desc'),
                 ), $sf_data->getRaw('sort'), '@users');
             ?>
-            
+
           </tr>
         </thead>
 
@@ -49,9 +50,10 @@ $csrf     = new sfForm();
             ?>
             <tr id="users-<?php echo $id ?>" class="<?php echo "$parity link users-$id " ?>">
               <td class="check"><input rel="item" type="checkbox" value="<?php echo $id ?>" name="ids[]"></td>
+              <td><?php echo $user->getUser()->username ?></td>
               <td><?php echo $user->first_name ?></td>
                 <td><?php echo $user->last_name ?></td>
-              <td><?php echo $user->email ?></td>            
+              <td><?php echo $user->email ?></td>
             </tr>
           <?php endforeach ?>
         </tbody>
@@ -68,9 +70,9 @@ $csrf     = new sfForm();
     </form>
 
     <?php include_partial('global/pager', array('pager' => $pager, 'route' => '@users')) ?>
-    
+
   <?php else: ?>
     <p><?php echo __('No results') ?></p>
   <?php endif ?>
-  
+
 </div>
