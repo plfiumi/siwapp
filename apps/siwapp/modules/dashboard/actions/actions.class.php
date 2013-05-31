2c009481fa26759d3e2290b662b58739ce05f48f
Sergi Almacellas Abellana <sergi@koolpi.com>
2013-05-31 18:14:44 +0200

    Fix error dobuled quotes on fiscality

--- apps/siwapp/modules/dashboard/actions/actions.class.php
+++ apps/siwapp/modules/dashboard/actions/actions.class.php
@@ -15,7 +15,7 @@ class dashboardActions extends sfActions
     $this->currency = $this->getUser()->getAttribute('currency');
     $this->namespace = 'invoices';
   }
-  
+
  /**
   * Executes index action
   *
@@ -30,8 +30,8 @@ class dashboardActions extends sfActions
     $company = Doctrine::getTable('Company')->find($company_id);
     $this->fiscality=$company->fiscality;
     if($this->fiscality){
-        //Incoming expenses grouped by Tax id. 
-        $q = InvoiceQuery::create()->innerJoin('i.Items it')->innerJoin('it.ItemTax itt')->innerJoin('it.Taxes t')
+        //Incoming expenses grouped by Tax id.
+        $q = InvoiceQuery::create()->innerJoin('i.Items it')->innerJoin('it.Taxes t')
             ->addSelect('t.name as name')->addSelect('t.value as value ')
             ->addSelect('i.id')->addSelect('it.id')->addSelect('t.id') //Necesary for doctrine
             ->addSelect('sum(
@@ -44,7 +44,7 @@ class dashboardActions extends sfActions
             ->addGroupBy('t.name')->addGroupBy('t.value');
         $this->fiscal_invoices = $q->fetchArray();
 
-        $q = ExpenseQuery::create()->innerJoin('i.Items it')->innerJoin('it.ItemTax itt')->innerJoin('it.Taxes t')
+        $q = ExpenseQuery::create()->innerJoin('i.Items it')->innerJoin('it.Taxes t')
             ->addSelect('t.name as name')->addSelect('t.value as value ')
             ->addSelect('i.id')->addSelect('it.id')->addSelect('t.id') //Necesary for doctrine
             ->addSelect('sum(
@@ -70,9 +70,9 @@ class dashboardActions extends sfActions
             ->addGroupBy('et.name');
         $this->detailed_expenses = $q->fetchArray();
     }
-    
+
     $q = InvoiceQuery::create()->Where('company_id = ?',$company_id )->search($search)->limit($this->maxResults);
-    
+
     $exp = ExpenseQuery::create()->Where('company_id = ?',$company_id )->search($search)->limit($this->maxResults);
 
     $eqp = EstimateQuery::create()->Where('status = 2')->AndWhere('company_id = ?',$company_id )->search($search)->limit($this->maxResults);
@@ -80,61 +80,61 @@ class dashboardActions extends sfActions
     $eqa = EstimateQuery::create()->Where('status = 3')->AndWhere('company_id = ?',$company_id )->search($search)->limit($this->maxResults);
 
     $eqr = EstimateQuery::create()->Where('status = 1')->AndWhere('company_id = ?',$company_id )->search($search)->limit($this->maxResults);
-    
+
     //Expenses total
     $this->epending  = $eqp->total('gross_amount');
-    if(empty($this->epending)) 
+    if(empty($this->epending))
         $this->epending = 0;
     $this->eapproved  = $eqa->total('gross_amount');
-    if(empty($this->eapproved)) 
+    if(empty($this->eapproved))
         $this->eapproved = 0;
     $this->erejected  = $eqr->total('gross_amount');
-    if(empty($this->erejected)) 
+    if(empty($this->erejected))
         $this->erejected = 0;;
-    
+
     // for the overdue unset the date filters, to show all the overdue
     unset($search['from'], $search['to']);
     $overdueQuery = InvoiceQuery::create()->Where('company_id = ?',$company_id )->search($search)->status(Invoice::OVERDUE);
 
     // totals
     $this->gross  = $q->total('gross_amount');
-    if(empty($this->gross)) 
+    if(empty($this->gross))
         $this->gross = 0;
     $this->due    = $q->total('due_amount');
-    if(empty($this->due)) 
+    if(empty($this->due))
         $this->due = 0;
     $this->paid   = $q->total('paid_amount');
-    if(empty($this->paid)) 
+    if(empty($this->paid))
         $this->paid = 0;
     $this->odue   = $overdueQuery->total('due_amount');
-    if(empty($this->odue)) 
+    if(empty($this->odue))
         $this->odue = 0;
     $this->taxes  = $q->total('tax_amount');
-    if(empty($this->taxes)) 
+    if(empty($this->taxes))
         $this->taxes = 0;
     $this->net    = $q->total('net_amount');
-    if(empty($this->net)) 
+    if(empty($this->net))
         $this->net = 0;
     //Expenses  totals
     $this->expense_gross  = $exp->total('gross_amount');
-    if(empty($this->expense_gross)) 
+    if(empty($this->expense_gross))
         $this->expense_gross = 0;
     $this->expense_due    = $exp->total('due_amount');
-    if(empty($this->expense_due)) 
+    if(empty($this->expense_due))
         $this->expense_due = 0;
     $this->expense_paid   = $exp->total('paid_amount');
-    if(empty($this->expense_paid)) 
+    if(empty($this->expense_paid))
         $this->expense_paid = 0;
     $this->expense_taxes  = $exp->total('tax_amount');
-    if(empty($this->expense_taxes)) 
+    if(empty($this->expense_taxes))
         $this->expense_taxes = 0;
     $this->expense_net    = $exp->total('net_amount');
-    if(empty($this->expense_net)) 
+    if(empty($this->expense_net))
         $this->expense_net = 0;
 
     // this is for the redirect of the payments forms
     $this->getUser()->setAttribute('module', $request->getParameter('module'));
-    
+
     // link counters
     $this->recentCounter  = $q->count();
     $this->overdueCounter = $overdueQuery->count();
@@ -142,9 +142,9 @@ class dashboardActions extends sfActions
     // recent & overdue invoices
     $this->recent         = $q->execute();
     $this->pending         = $eqp->execute();
-    
+
     $this->net=$q->total('net_amount');
     $this->expense_net=$exp->total('net_amount');
   }
-  
+
 }
