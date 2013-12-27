6e901218ef81807692cd6081ee8ff292bcf8978f
Sergi Almacellas Abellana <sergi@koolpi.com>
2013-12-27 12:08:36 +0100

    Don't load default taxes on expenses

--- apps/siwapp/modules/common/templates/_expenseRow.php
+++ apps/siwapp/modules/common/templates/_expenseRow.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 use_helper('jQuery', 'Number', 'JavascriptBase');
 $currency = $sf_user->getAttribute('currency');
 ?>
@@ -13,7 +13,7 @@ $currency = $sf_user->getAttribute('currency');
     <?php echo $invoiceItemForm->renderHiddenFields();?>
     <?php echo $invoiceItemForm['description']->render(array(), ESC_RAW) ?>
   </td>
-  <td class="right ucost"><?php 
+  <td class="right ucost"><?php
     if ($invoiceItemForm['expense_type_id']->hasError()):
       echo $invoiceItemForm['expense_type_id']->render(array('class'=>'error'), ESC_RAW);
     else:
@@ -21,14 +21,14 @@ $currency = $sf_user->getAttribute('currency');
     endif;
     echo javascript_tag("if($('#".$invoiceItemForm['expense_type_id']->renderId()."').val() == '') $('#".$invoiceItemForm['expense_type_id']->renderId()."').val($('#expense_default_expense_type').val())");
     ?></td>
-  <td class="right quantity"><?php 
+  <td class="right quantity"><?php
     if ($invoiceItemForm['unitary_cost']->hasError()):
       echo $invoiceItemForm['unitary_cost']->render(array('class'=>'error'), ESC_RAW);
     else:
       echo $invoiceItemForm['unitary_cost']->render(array(), ESC_RAW);
     endif;
   ?></td>
-  <td class="right quantity"><?php 
+  <td class="right quantity"><?php
     if ($invoiceItemForm['quantity']->hasError()):
       echo $invoiceItemForm['quantity']->render(array('class'=>'error'), ESC_RAW);
     else:
@@ -38,7 +38,7 @@ $currency = $sf_user->getAttribute('currency');
   <td class="right taxes_td">
     <span id="<?php echo $rowId?>_taxes" class="taglist taxes">
       <?php $err = $invoiceItemForm['taxes_list']->hasError() ? 'error' : '';
-        $item_taxes = $invoiceItemForm['taxes_list']->getValue() ? 
+        $item_taxes = $invoiceItemForm['taxes_list']->getValue() ?
           $invoiceItemForm['taxes_list']->getValue() : array();
         $totalTaxesValue = Doctrine::getTable('Tax')->getTotalTaxesValue($item_taxes);
         echo jq_javascript_tag("
@@ -55,37 +55,8 @@ $currency = $sf_user->getAttribute('currency');
         include_partial('common/taxExpenseSpan',array('taxKey'=>$taxId,'rowId'=>$rowId,'err'=>$err));
         endforeach?>
 
-      <?php
-        // if item has no taxes, and there are "default" taxes defined, we add them
-        if(isset($isNew) && !count($item_taxes))
-        {
-          $default_taxes = Doctrine::getTable('Tax')->createQuery()->
-            AndWhere('active = ? ', true)->
-            AndWhere('is_default = ? ', true)->
-            AndWhere('company_id = ?', sfContext::getInstance()->getUser()->getAttribute('company_id'))->
-execute();
-          foreach($default_taxes as $taxx)
-          {
-            echo javascript_tag(
-                   jq_remote_function(
-                     array(
-                       'update'=> $rowId.'_taxes',
-                       'url'  => 'common/ajaxAddExpenseItemTax',
-                       'position' => 'bottom',
-                       'method'   => 'post',
-                       'with'     => "{
-                                        item_tax_index:   new_item_tax_index++, 
-                                        invoice_item_key: '$rowId',
-                                        selected_tax:     '".$taxx->id."'
-                                      }"
-                       )
-                     )
-                   );
-          }
-        }
-      ?>
     </span>
-    
+
   </td>
     <td class="right price"><?php echo format_currency(Tools::getRounded(Tools::getNetAmount($invoiceItemForm['unitary_cost']->getValue(),$invoiceItemForm['quantity']->getValue(),0,$totalTaxesValue), Tools::getDecimals()), $currency) ?> </td>
 </tr>
@@ -100,8 +71,8 @@ echo javascript_tag("
         var parsed = [];
         for (key in data) {
           parsed[parsed.length] = { data: [
-            data[key].description, 
-            data[key].reference, 
+            data[key].description,
+            data[key].reference,
             data[key].price,
             data[key].id
           ], value: data[key].description, result: data[key].description };
@@ -118,7 +89,7 @@ echo javascript_tag("
       $('#".$invoiceItemForm['product_id']->renderId()."').val(item[3]);
       $(document).trigger('GlobalUpdateEvent');
     });
-    
+
   //connect the selection of a product to update the row item
   $('#".$invoiceItemForm['product_autocomplete']->renderId()."')
     .autocomplete('".$urlAjaxSelectProduct."', jQuery.extend({}, {
@@ -128,7 +99,7 @@ echo javascript_tag("
         for (key in data) {
           parsed[parsed.length] = { data: [
             data[key].description,
-            data[key].reference,  
+            data[key].reference,
             data[key].price,
             data[key].id
           ], value: data[key].description, result: data[key].description };
