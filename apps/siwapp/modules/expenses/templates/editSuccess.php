9387c413f0f50bd68b481dec1e3e4a8f1cd3c686
Sergi Almacellas Abellana <sergi@koolpi.com>
2013-12-27 12:18:26 +0100

    Make supplier reference required on expenses

--- apps/siwapp/modules/expenses/templates/editSuccess.php
+++ apps/siwapp/modules/expenses/templates/editSuccess.php
@@ -6,33 +6,33 @@ include_javascripts_for_form($invoiceForm);
 $invoice = $invoiceForm->getObject();
 ?>
 <div id="invoice-container" class="content">
-  
+
   <h2><?php echo $title ?></h2>
 
   <form action="<?php echo url_for("expenses/$action") ?>" method="post" <?php $invoiceForm->isMultipart() and print 'enctype="multipart/form-data" ' ?> class="invoice">
     <input type="hidden" id="send_email" name="send_email" value="0" />
 
   <div id="saving-options" class="block">
-    <?php 
+    <?php
     if ($invoice->getId()) {
       echo gButton_to(__('Delete'), "expenses/delete?id=" . $invoice->getId(), array(
-        'class' => 'action delete', 
+        'class' => 'action delete',
         'post' => true,
         'confirm' => __('Are you sure?'),
-        ) , 'button=false')."&nbsp;&nbsp;&nbsp;&nbsp;"; 
-        echo gButton_to_function(__('Print'), "Tools.popup(siwapp_urls.printHtml + '?ids[]=".$invoice->getId()."')", 'class=action print'); 
-      echo gButton_to_function(__('Save PDF'), "window.location=siwapp_urls.printPdf + '?ids[]=".$invoice->getId()."'", 'class=action pdf')."&nbsp;&nbsp;&nbsp;&nbsp;"; 
+        ) , 'button=false')."&nbsp;&nbsp;&nbsp;&nbsp;";
+        echo gButton_to_function(__('Print'), "Tools.popup(siwapp_urls.printHtml + '?ids[]=".$invoice->getId()."')", 'class=action print');
+      echo gButton_to_function(__('Save PDF'), "window.location=siwapp_urls.printPdf + '?ids[]=".$invoice->getId()."'", 'class=action pdf')."&nbsp;&nbsp;&nbsp;&nbsp;";
     }
 
     if ($invoice->isNew() || $db_draft)
     {
       echo gButton_to_function(__('Save as draft'), "\$('form.invoice').saveInvoiceAsDraft()", 'class=action save-draft', 'button=true')."&nbsp;&nbsp;";
     }
-    
-    echo gButton(__('Save'), 'type=submit class=action primary save', 'button=true'); 
+
+    echo gButton(__('Save'), 'type=submit class=action primary save', 'button=true');
     ?>
-  </div>    
-  <?php 
+  </div>
+  <?php
     echo $invoiceForm['id'];
     // here draft, in case were saved as draft, the button must put 1 here
     echo $invoiceForm['draft'];
@@ -46,19 +46,19 @@ $invoice = $invoiceForm->getObject();
   <ul id="status">
     <li><?php echo __('Status')?>: <span class="status <?php echo ($stat = $invoice->getStatusString()) ?>"><?php echo __($stat)?></span></li>
   </ul>
-  
-  <?php 
+
+  <?php
     include_partial('common/globalErrors', array('form' => $invoiceForm));
     include_partial('common/supplierInfo', array('invoiceForm' => $invoiceForm)); ?>
-    
+
   <div id="payment-data" class="block">
     <h3><?php echo __('Payment details') ?></h3>
     <ul class="inline">
+      <?php echo $invoiceForm['supplier_reference']->renderRow() ?>
       <?php echo $invoiceForm['issue_date']->renderRow() ?>
       <?php echo $invoiceForm['due_date']->renderRow() ?>
-      <?php echo $invoiceForm['supplier_reference']->renderRow() ?>
     </ul>
-      
+
     <?php include_partial('common/items', array(
       'invoice' => $invoice,
       'invoiceForm' => $invoiceForm,
@@ -70,24 +70,24 @@ $invoice = $invoiceForm->getObject();
   <?php include_partial('common/tagsDataBlock', array('invoice' => $invoice, 'invoiceForm' => $invoiceForm)) ?>
 
   <div id="saving-options" class="block">
-    <?php 
+    <?php
     if ($invoice->getId()) {
       echo gButton_to(__('Delete'), "expenses/delete?id=" . $invoice->getId(), array(
-        'class' => 'action delete', 
+        'class' => 'action delete',
         'post' => true,
         'confirm' => __('Are you sure?'),
-        ) , 'button=false')."&nbsp;&nbsp;&nbsp;&nbsp;"; 
-        echo gButton_to_function(__('Print'), "Tools.popup(siwapp_urls.printHtml + '?ids[]=".$invoice->getId()."')", 'class=action print'); 
-      echo gButton_to_function(__('Save PDF'), "window.location=siwapp_urls.printPdf + '?ids[]=".$invoice->getId()."'", 'class=action pdf')."&nbsp;&nbsp;&nbsp;&nbsp;"; 
+        ) , 'button=false')."&nbsp;&nbsp;&nbsp;&nbsp;";
+        echo gButton_to_function(__('Print'), "Tools.popup(siwapp_urls.printHtml + '?ids[]=".$invoice->getId()."')", 'class=action print');
+      echo gButton_to_function(__('Save PDF'), "window.location=siwapp_urls.printPdf + '?ids[]=".$invoice->getId()."'", 'class=action pdf')."&nbsp;&nbsp;&nbsp;&nbsp;";
     }
 
     if ($invoice->isNew() || $db_draft)
     {
       echo gButton_to_function(__('Save as draft'), "\$('form.invoice').saveInvoiceAsDraft()", 'class=action save-draft', 'button=true')."&nbsp;&nbsp;";
     }
-    
-    echo gButton(__('Save'), 'type=submit class=action primary save', 'button=true'); 
+
+    echo gButton(__('Save'), 'type=submit class=action primary save', 'button=true');
     ?>
-  </div> 
+  </div>
   </form>
 </div>
--- lib/form/doctrine/ExpenseForm.class.php
+++ lib/form/doctrine/ExpenseForm.class.php
@@ -15,20 +15,20 @@ class ExpenseForm extends CommonForm
   {
     unset($this['number'], $this['created_at'], $this['updated_at']);
     // we unset paid_amount so the system don't "nullify" the field on every expense editing.
-    unset($this['paid_amount']); 
+    unset($this['paid_amount']);
     unset($this['shipping_address']);
-    
+
     $this->number = $this->getObject()->getNumber();
-    $this->widgetSchema['issue_date'] = 
+    $this->widgetSchema['issue_date'] =
       new sfWidgetFormI18nJQueryDate($this->JQueryDateOptions);
-    $this->widgetSchema['due_date']   = 
+    $this->widgetSchema['due_date']   =
       new sfWidgetFormI18nJQueryDate($this->JQueryDateOptions);
     $this->widgetSchema['draft']      = new sfWidgetFormInputHidden();
     $this->widgetSchema['default_expense_type']      = new sfWidgetFormInputHidden();
     $this->widgetSchema['closed']->setLabel('Force to be closed');
 
     $this->widgetSchema['sent_by_email']->setLabel('Sent by email');
-    
+
     $this->setDefaults(array(
       'issue_date'              => time(),
       'draft'                   => 0
@@ -37,7 +37,7 @@ class ExpenseForm extends CommonForm
 
     $this->validatorSchema['supplier_email'] = new sfValidatorEmail(
                                                      array(
-                                                       'max_length' => 100, 
+                                                       'max_length' => 100,
                                                        'required' => false
                                                        ),
                                                      array(
@@ -46,15 +46,16 @@ class ExpenseForm extends CommonForm
                                                      );
 
     $this->validatorSchema['supplier_name']  = new sfValidatorString(array('required' => true));
+    $this->validatorSchema['supplier_reference']  = new sfValidatorString(array('required' => true));
     $this->validatorSchema['default_expense_type'] = new sfValidatorPass();
-        
+
     $this->widgetSchema->setNameFormat('expense[%s]');
     parent::configure(true);
-    //Override validations: 
+    //Override validations:
     $this->validatorSchema['customer_email'] = new sfValidatorString(array('max_length' => 100, 'required' => false));
     $this->validatorSchema['customer_name']  =  new sfValidatorString(array('max_length' => 100, 'required' => false));
     $this->validatorSchema['series_id'] = new sfValidatorDoctrineChoice(
-            array('model' => $this->getRelatedModelName('Series'), 
+            array('model' => $this->getRelatedModelName('Series'),
             'required' => false));
   }
 
@@ -62,5 +63,5 @@ class ExpenseForm extends CommonForm
   {
     return 'Expense';
   }
-  
+
 }
