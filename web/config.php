feafbff6970b734c059b69bb2542597cdcd4813a
plfiumi <plfiumi@gmail.com>
2014-11-09 20:15:02 -0300

    rollback config

--- web/config.php
+++ web/config.php
@@ -1,4 +1,4 @@
 <?php
-$sw_installed = true;
+$sw_installed = false;
 $options['sf_web_dir'] = dirname(__FILE__);
 $options['sf_root_dir'] = realpath($options['sf_web_dir'].DIRECTORY_SEPARATOR.'..');
